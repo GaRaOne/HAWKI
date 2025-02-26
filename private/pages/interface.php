194af90389452e3f2b17d1898c9031b62164acc8
garaone <garaone@co3.de>
2025-02-26 10:30:20 +0100

    change feedback to ki-support email

--- private/pages/interface.php
+++ private/pages/interface.php
@@ -184,7 +184,7 @@
 					</svg>
 				</div>
 				<div class="info">
-					<a href="#" id="feedback" onclick="load(this, 'feedback_loader.php')"><?php echo $translation["FeedBack"]; ?></a>
+					<a href="mailto:ki-support@b-tu.de" id="feedback"><?php echo $translation["FeedBack"]; ?></a>
 					<a href="logout"><?php echo $translation["SignOut"]; ?></a>
 					<br>
 					<!-- CHANGE THIS PART TO ONCLICK EVENT TO LOAD THE PAGE IN MESSAGES PANEL.
